6db061c59b389f381c967e8a090044376ad72acd
roal518 <roalqwerty@gmail.com>
2023-09-25 13:18:33 +0900

    png size controll
    크기를 약간 키웠다. (프로그램에서 키우지 않고 파일에서 바로 키웠음)

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -3,6 +3,21 @@ from pico2d import *
 TUK_WIDTH,TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH,TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.PNG')
-IDLE_character = load_image('woodcutter_idle.png')
-MOVING_character=load_image('woodcutter_run.png')
+IDLE_character = load_image('Woodcutter_idle.png')
+MOVING_character=load_image('Woodcutter_run.png')
 
+def handle_events():
+    global running
+    global x, y, delay
+running = True
+moving = True
+frame = 0
+x, y = TUK_WIDTH // 2 ,  TUK_HEIGHT // 2
+hide_cursor()
+while running:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
+    if moving:
+        MOVING_character.clip_draw(frame*100)
+    elif not moving:
+        IDLE_character.clip_draw(frame*100)
