d661d9be37bd307798afd6937e458c08f86509f0
roal518 <roalqwerty@gmail.com>
2023-09-25 17:12:46 +0900

    check frame-changed

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -11,22 +11,33 @@ MOVE_character.clip_draw(0,0,100,102,1280,940)
 # x,y 좌표에서 캐릭터가 제대로 보이는 최댓값
 
 def handle_events():
-    global running
+    global running, xdir
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        if event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_LEFT:
+            xdir -=1
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_RIGHT:
+            xdir +=1
 
+
+x=800//2
 running = True
-frame = 0
+xdir = 0
+ydir = 0
+runframe = 0
+idleframe = 0
 while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
+    MOVE_character.clip_draw(runframe*100, 0, 100, 102,x, 200)
     update_canvas()
     handle_events()
-    frame = (frame+1) % 8
+    x += xdir*5
+    runframe = (runframe+1) % 6
     delay(0.05)
 
 close_canvas()
