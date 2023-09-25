8c6becf04d5d1502b7143ad15d4ed45fcbcf3cb3
roal518 <roalqwerty@gmail.com>
2023-09-25 17:38:44 +0900

    make diff btw stop-move

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -11,7 +11,8 @@ MOVE_character.clip_draw(0,0,100,102,1280,940)
 # x,y 좌표에서 캐릭터가 제대로 보이는 최댓값
 
 def handle_events():
-    global running, xdir, ydir
+    global running, moving
+    global xdir, ydir
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -21,23 +22,32 @@ def handle_events():
                 running = False
             elif event.key == SDLK_LEFT:
                 xdir -=1
+                moving = True
             elif event.key == SDLK_RIGHT:
                 xdir +=1
+                moving = True
             elif event.key == SDLK_UP:
                 ydir += 1
+                moving = True
             elif event.key == SDLK_DOWN:
                 ydir -= 1
+                moving = True
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_LEFT:
                 xdir +=1
+                moving = False
             elif event.key == SDLK_RIGHT:
                 xdir -=1
+                moving = False
             elif event.key == SDLK_UP:
                 ydir -= 1
+                moving = False
             elif event.key == SDLK_DOWN:
                 ydir += 1
-x=TUK_WIDTH//2
-y=TUK_HEIGHT//2
+                moving = False
+
+x = TUK_WIDTH//2
+y = TUK_HEIGHT//2
 running = True
 moving = False
 xdir = 0
@@ -47,11 +57,16 @@ idleframe = 0
 while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
-    MOVE_character.clip_draw(runframe*100, 0, 100, 102, x, y)
+    if moving:
+        MOVE_character.clip_draw(runframe*100, 0, 100, 102, x, y)
+    else:
+        IDLE_character.clip_draw(idleframe*100, 0, 100, 100, x, y)
     update_canvas()
     handle_events()
     x += xdir*5
     y += ydir*5
+    
+    idleframe = (runframe+1) % 4
     runframe = (runframe+1) % 6
     delay(0.05)
 
