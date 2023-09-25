6dc0ee9747d538de443ef33381fe48cd444df77c
roal518 <roalqwerty@gmail.com>
2023-09-25 13:05:32 +0900

    added file

---
+++ move_my_character_with_key.py
@@ -0,0 +1,8 @@
+from pico2d import *
+
+TUK_WIDTH,TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH,TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.PNG')
+IDLE_character = load_image('woodcutter_idle.png')
+MOVING_character=load_image('woodcutter_run.png')
+
